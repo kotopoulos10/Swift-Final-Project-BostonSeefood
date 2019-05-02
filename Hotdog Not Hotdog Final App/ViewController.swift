3ccd46283fb4a86686e2845850757d8b4bdd9896
Tom Kotopoulos <kotopoulos10@gmail.com>
2019-05-02 13:12:22 -0400

    Initial Commit

---
+++ Hotdog Not Hotdog Final App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hotdog Not Hotdog Final App
+//
+//  Created by Tom Kotopoulos on 5/2/19.
+//  Copyright © 2019 Tom Kotopoulos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
